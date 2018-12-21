95d4a01556fcde3cf19f0bdb590ea87a938786dd
Eric James <eric@verdureanalytics.com>
2018-12-21 17:58:24 -0500

    Headless Fixes

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import WebDriverException
+from pyvirtualdisplay import Display
 from urllib.error import URLError
 # from selenium.common.exceptions import AttributeError
 import numpy as np
@@ -21,17 +22,22 @@ import bs4
 import os
 import sys
 
-delay = 3
-firefox_options = Options()
-# chrome_options.add_argument('--incognito')
-firefox_options.add_argument('--headless')
+with Display():
+    delay = 3
+    firefox_options = Options()
+    # chrome_options.add_argument('--incognito')
+    firefox_options.add_argument('--headless')
+    
+    driver = webdriver.Firefox(executable_path='driver/geckodriver', options=firefox_options)
+    driver.implicitly_wait(2)
+    
+    try:
+        driver.get('https://www.nadaguides.com/Boats/2017/Grady-White-Boats/180-FISHERMAN-CC_/32072308/Specs')
+    
+        engine_manufacturer = driver.find_element_by_xpath("//*[contains(text(), 'Engine Manufacturer')]")
+        engine_parent = engine_manufacturer.find_element_by_xpath('..')
+        print(engine_parent.text)
 
-driver = webdriver.Firefox(executable_path='driver/geckodriver', options=firefox_options)
-driver.implicitly_wait(2)
-
-driver.get('https://www.nadaguides.com/Boats/2017/Grady-White-Boats/180-FISHERMAN-CC_/32072308/Specs')
-
-engine_manufacturer = driver.find_element_by_xpath("//*[contains(text(), 'Engine Manufacturer')]")
-engine_parent = engine_manufacturer.find_element_by_xpath('..')
-print(engine_parent.text)
+    finally:
+        driver.quit()
 
