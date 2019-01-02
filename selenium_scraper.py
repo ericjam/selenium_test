c214f5558c21b646ee554d5fe467a3972caadde8
Eric James <eric@verdureanalytics.com>
2019-01-02 11:26:33 -0500

    Latest

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -1,6 +1,5 @@
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,13 +9,11 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import WebDriverException
 from pyvirtualdisplay import Display
 from urllib.error import URLError
-# from selenium.common.exceptions import AttributeError
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
 import time
 import random
-#import progressbar
 from bs4 import BeautifulSoup
 import bs4
 import os
@@ -25,21 +22,25 @@ import sys
 with Display():
 
     delay = 3
-    # firefox_options = Options()
-    # chrome_options.add_argument('--incognito')
-    # firefox_options.add_argument('--headless')
     
-    # driver = webdriver.Firefox(options=firefox_options)
     driver = webdriver.Firefox()
-    # driver = webdriver.Firefox(executable_path='driver/geckodriver', options=firefox_options)
     driver.implicitly_wait(2)
     
     try:
-        driver.get('https://www.nadaguides.com/Boats/2017/Grady-White-Boats/180-FISHERMAN-CC_/32072308/Specs')
+
+        # driver.get('https://www.nadaguides.com/Boats/2017/Grady-White-Boats/180-FISHERMAN-CC_/32072308/Specs')
+        # driver.get('https://www.nadaguides.com/Boats/2017/Grady-White-Boats')
+        driver.get('https://www.boattrader.com/browse/pleasure-boats/florida/')
     
-        engine_manufacturer = driver.find_element_by_xpath("//*[contains(text(), 'Engine Manufacturer')]")
-        engine_parent = engine_manufacturer.find_element_by_xpath('..')
-        print(engine_parent.text)
+        boat_list = driver.find_element_by_class_name('browse-listings-photo')
+
+        for i in boat_list.find_elements_by_css_selector('a'):
+            print(i.text)
+
+
+        # engine_manufacturer = driver.find_element_by_xpath("//*[contains(text(), 'Engine Manufacturer')]")
+        # engine_parent = engine_manufacturer.find_element_by_xpath('..')
+        # print(engine_parent.text)
 
     finally:
         driver.quit()
